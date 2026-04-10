9de954c4a32d9aca8f6ba6969a9255926a221f5e
charvi-web <charvisingh077@gmail.com>
2026-04-10 23:49:14 +0530

    Completed questions on one db

--- hard.sql
+++ hard.sql
@@ -65,4 +65,106 @@ GROUP BY
     WHEN patient_id % 2 = 0 THEN 'Yes'
     ELSE 'No'
   END
-  
+
+
+
+
+
+
+
+
+  SELECT N.province_name
+FROM province_names N
+  JOIN patients P ON P.province_id = N.PROVINCE_ID
+group by
+  N.province_name HAVING COUNT(
+    CASE
+      WHEN P.gender = 'M' THEN 1
+    END
+  ) > COUNT(
+    CASE
+      WHEN P.gender = 'F' THEN 1
+    END
+  )
+
+
+
+
+
+
+
+
+  SELECT *
+FROM patients
+WHERE
+  first_name LIKE '__r%'
+  AND gender = 'F'
+  AND month(birth_date) IN (2, 5, 12)
+  AND weight between 60 and 80
+  AND patient_id % 2 = 1
+  AND city = 'Kingston';
+
+
+
+
+
+
+
+
+
+  select
+  CONCAT(ROUND(
+    count(
+      case
+        when gender = 'M' THEN 1
+      END
+    ) * 100.0 / count(*),
+    2
+  ),'%') AS percent_of_male_patients
+FROM patients
+
+
+
+
+
+
+
+SELECT
+  admission_date,
+  COUNT(admission_date) AS admission_day,
+  COUNT(admission_date) - LAG(count(admission_date)) over (
+    order by
+      admission_date
+  ) AS admission_count_change
+FROM admissions
+group by admission_date
+
+
+
+
+
+
+
+-- NOS ARE KEPT ABOVE WORDS IN ENGLISH
+select province_name
+FROM province_names
+order by
+  CASE
+    WHEN province_name = 'Ontario' THEN 1
+    ELSE province_name
+  END;
+
+
+
+SELECT
+  D.doctor_id,
+  CONCAT(D.first_name, ' ', D.last_name) AS doctor_name,
+  D.specialty,
+  YEAR(admission_date) AS selected_year,
+  COUNT(*) AS total_admissions
+FROM doctors D
+  JOIN admissions A ON D.doctor_id = A.attending_doctor_id
+group by
+  D.doctor_id,
+  YEAR(A.admission_date)
+
