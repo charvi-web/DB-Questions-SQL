a47773cea5370e5c703e022fe56abb4da6c8f2ad
charvi-web <charvisingh077@gmail.com>
2026-04-23 00:00:55 +0530

    Aggregate queries on leetcode sql completed

---
+++ TOP 50 SQL/AGGREGATE/gameplayanalysis.sql
@@ -0,0 +1,7 @@
+SELECT ROUND(  (COUNT(DISTINCT A.player_id))/(SELECT COUNT(DISTINCT player_id) FROM Activity),2)
+AS fraction
+FROM Activity A 
+JOIN (
+    SELECT player_id, MIN(event_date) as first_date FROM Activity GROUP BY player_id
+) B 
+ON A.player_id = B.player_id WHERE A.event_date = B.first_date+1
